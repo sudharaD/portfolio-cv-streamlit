f97e40f34d75c593e97141736655852153f33345
sudharaD <72960779+sudharaD@users.noreply.github.com>
2023-06-09 19:26:31 +0530

    Hero Section Completed

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 
 
 css_file = current_dir/"styles"/"main.css"
-resume_file = current_dir/"assests"/"CV.pdf"
-profile_pic = current_dir/"assests"/"profile-pic.png"
+resume_file = current_dir/"assets"/"CV.pdf"
+profile_pic = current_dir/"assets"/"profile-pic.png"
 
 
 # --- General Settings - Part 2 ---
@@ -45,4 +45,26 @@ PROJECTS = {
 }
 
 
-st.title("Hello there")
+# --- LOAD CSS, PDF & PROFILE PICTURE ---
+with open(css_file) as f:
+    st.markdown("<style>{}<style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile_pic)
+
+
+# --- Hero Section ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image(profile_pic, width=230)
+
+with col2:
+    st.title(NAME)
+    st.write(DESCRIPTION)
+    st.download_button(
+        label=" 📄 Download Resume",
+        data=PDFbyte,
+        file_name = resume_file.name,
+        mime="application/octet-stream",
+    )
+    st.write(":mailbox:", EMAIL)
