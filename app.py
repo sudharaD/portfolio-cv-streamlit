a25e0075ee835fd4407f7996b93aacc570a02109
sudharaD <72960779+sudharaD@users.noreply.github.com>
2023-07-17 11:00:03 +0530

    pic update 4

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 
 css_file = current_dir/"styles"/"main.css"
 resume_file = current_dir/"assets"/"Sudhara Dhananjaya - CV.pdf"
-profile_pic = current_dir/"assets"/"profile-pic-4.png"
+profile_pic = current_dir/"assets"/"profile-pic-3.png"
 
 
 # --- General Settings - Part 2 ---
